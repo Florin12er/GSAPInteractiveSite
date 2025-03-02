77542ea5234e4e2454d5acdf292e92a463ce0acf
Florin.12er <sebastianflorin603@gmail.com>
2025-03-02 23:34:55 +0100

    added project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import About from "./components/About";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar.jsx";
+import Features from "./components/Features.jsx";
+
+function App() {
+    return (
+        <main className="relative min-h-screen w-screen overflow-x-hidden">
+            <Navbar/>
+            <Hero/>
+            <About/>
+            <Features/>
+        </main>
+    );
+}
+
+export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,31 @@
+import clsx from "clsx";
+
+import PropTypes from "prop-types";
+
+const Button = ({ title, id, rightIcon, leftIcon, containerClass }) => {
+  return (
+    <button
+      id={id}
+      className={clsx(
+        "group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ",
+        containerClass
+      )}
+    >
+      {leftIcon}
+      <span className="relative inline-flex overflow-hidden font-general text-xs uppercase">
+        <div>{title}</div>
+      </span>
+      {rightIcon}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  title: PropTypes.string.isRequired,
+  id: PropTypes.string,
+  rightIcon: PropTypes.node,
+  leftIcon: PropTypes.node,
+  containerClass: PropTypes.string,
+};
+
+export default Button;
